8bec292fcff7c4314964b7768632d692c21366ee
Marbin3d <marbin3d@hotmail.com>
2017-05-01 11:37:17 +1000

    script confirm button to go to siteTrees page

--- js/siteSpecification.js
+++ js/siteSpecification.js
@@ -3,7 +3,6 @@ const confirm = document.querySelector('.confirm');
 const back = document.querySelector('.back');
 
 
-
 cancel.addEventListener('click', function(){
   window.location.href = 'site.html';
 });
@@ -11,3 +10,26 @@ cancel.addEventListener('click', function(){
 back.addEventListener('click', function(){
   window.location.href = 'site.html';
 });
+
+
+//fordwards
+
+//go from siteSpecification to siteTrees
+confirm.addEventListener('click', function(){	
+	
+	window.location.href = 'siteTrees.html'; 
+});
+
+//shows data uploaded feedback
+function upldDatSiteSpec(siteLength, siteWidth,siteSection){	
+	// if (e.preventDefault) e.preventDefault();	
+	alert("Site specifications saved");	
+	siteLength.empty();
+	siteWidth.value="";
+	siteSection.value="";
+	window.location.href = 'siteTrees.html';
+	return false;	
+	
+}
+
+// backwards
