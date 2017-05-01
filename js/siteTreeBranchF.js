d904bd4899b8ecaf104248cd1781de258ec3567f
Marbin3d <marbin3d@hotmail.com>
2017-05-01 11:32:09 +1000

    removed due to changed of file name

--- js/siteTreeBranchF.js
+++
@@ -1,8 +0,0 @@
-
-const ok = document.querySelector('#accept');
-
-//Javascript fordward functionalities
-ok.addEventListener('click',function goToSiteTreeBranch() {
-
-  window.location.href = "siteTrees.html";
-});
