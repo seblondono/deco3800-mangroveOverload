7101cc7d7aedae1b9209eb25a67fd1ff2cbf6a7b
Alex Liu <ljd6106@gmail.com>
2017-05-29 19:25:27 +1000

    Linking siteTree to tree quick view pages

--- js/setTree.js
+++ js/setTree.js
@@ -2,6 +2,8 @@ const cancel = document.querySelector('.addTree');
 const confirm = document.querySelector('.finish');
 const back = document.querySelector('.back');
 
+const quickreview1 = document.querySelector('.treeDetails1');
+const quickreview2 = document.querySelector('.treeDetails');
 
 
 cancel.addEventListener('click', function(){
@@ -14,4 +16,12 @@ confirm.addEventListener('click', function(){
 
 back.addEventListener('click', function(){
   window.location.href = 'site.html';
+});
+
+quickreview1.addEventListener('click', function(){
+  window.location.href = 'treeQuickReview.html';
+});
+
+quickreview2.addEventListener('click', function(){
+  window.location.href = 'treeQuickReview.html';
 });
--- js/siteReview.js
+++ js/siteReview.js
@@ -1,8 +1,16 @@
 
 const back = document.querySelector('.cancel');
-
-
+const review = document.querySelector('.yearBtn1');
+const click = document.querySelector('.yearBtn2');
 
 back.addEventListener('click', function(){
   window.location.href = 'site.html';
+});
+
+review.addEventListener('click', function(){
+  window.location.href = 'siteYearReview.html';
+});
+
+click.addEventListener('click', function(){
+  window.location.href = 'siteYearReview.html';
 });
