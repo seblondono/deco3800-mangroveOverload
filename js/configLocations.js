9c2f1ac85f63ed03caa7e2672683188a1ac244f4
Sebastian Londono Mesa <sebastianlondonom@gmail.com>
2017-05-31 18:42:33 +1000

    Changed markup for configLocations.html to only display the name of the location, edit button and delete button. Added forward and backward navigation to configLocations.html in configLocations.js Added functionality to edit button. Delete button remaining. Added styling to configLocations.html in configLocations.css

--- js/configLocations.js
+++ js/configLocations.js
@@ -1,74 +1,17 @@
-// Login button in index.html
-const login = document.querySelector('#loginButton');
-// Numerical keypad in login.html
-const buttons = document.querySelectorAll('.number');
-// Password boxes in login.html
-const keyBoxes = document.querySelectorAll('.keyBox');
+var addLocation = document.querySelector('.addLocation > button');
+var editLocation = document.querySelectorAll('.edit');
+var back = document.querySelector('.back');
 
-const btnSetting= document.querySelector('#btn_Setting');
+console.log(editLocation);
 
+editLocation.forEach(editButton => editButton.addEventListener('click', addLocations));
 
-// Access password Setting
-const password = '123456';
-// Password as typed by user in keypad at configLogin.html
-const keyEnter = [];
+addLocation.addEventListener('click', addLocations);
 
-// From index.html to login.html
-function goToPasword() {
-  window.location.href = 'login.html';
+function addLocations() {
+  window.location.href = 'configAddLocation.html';
 }
 
-// From index.html to  setting.html
-function goToPaswordConfigLogin() {
-  window.location.href = 'configLogin.html';
-}
-
-// Identifies the keypad numbers pressed and allows access to the app
-function keyPressed(e) {
-  // user hit delete
-  if (e.target.textContent == '') {
-    drawKeyBoxes('delete');
-    keyEnter.pop();
-  } else if(e.target.textContent == 'Cancel'){ // user hits cancel
-    window.location.href = 'index.html';
-  } else { // user hits a number key
-    keyEnter.push(e.target.textContent);
-    // removes the first key entered to keep length in range of password length
-    keyEnter.splice(-password.length-1, keyEnter.length - password.length);
-
-    drawKeyBoxes('add');
-
-    // checks if keys entered match password
-    if (keyEnter.join('').includes(password)) {
-      window.location.href = 'configMenu.html';
-    }
-  }
-}
-
-// draws an * in the keyboxes at login.html
-function drawKeyBoxes(action) {
-  for (var i = 0; i < keyEnter.length; i++) {
-    if (keyBoxes[i].textContent == '' && action == 'add') {
-      keyBoxes[i].textContent = '*';
-    } else if (keyBoxes[i].textContent == '*' && action == 'delete') {
-      keyBoxes[keyEnter.length - 1].textContent = '';
-    }
-  }
-}
-
-// listener for login button at index.html
-if (login != null) {
-  login.addEventListener('click', goToPasword);
-}
-
-
-// listener for setting btn at index.html
-if (btnSetting != null) {
-  btnSetting.addEventListener('click', goToPaswordConfigLogin);
-}
-
-
-
-
-// listener for keypad buttons at indes.html
-buttons.forEach(button => button.addEventListener('click', keyPressed));
+back.addEventListener('click', function() {
+  window.location.href = 'configMenu.html';
+});
