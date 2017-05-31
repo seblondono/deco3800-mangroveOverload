218988ee65cfd0ada3bc2d3c8a0ea9489e9caa7e
Sebastian Londono Mesa <sebastianlondonom@gmail.com>
2017-05-31 20:59:51 +1000

    Minor change of class name in configAddSite.html Changed markup of configAddSpecies.html so it agrees with the styling of the whole project. Removed class from addLocation button in configLocations.html Removed class from addLocation button in configSites.html and change value of alt text. Changed markup of configSpecies.html so it agrees with the styling of the whole project. Added styling to configSpecies.html in configSpecies.css. Added styling to configAddSpecies.html in configAddSpecies.css. Added functionality to configAddSpecies.html. Now the back button, cancel and accept function. Added functionality to configSpecies.html. Now there is forward and backward navigation.

--- js/configAddSpecies.js
+++ js/configAddSpecies.js
@@ -2,54 +2,15 @@ const cancel = document.querySelector('#cancel');
 const accept = document.querySelector('#accept');
 const back = document.querySelector('.back');
 
-
-//Javascript fordward functionalities and validation
-
-//accept.addEventListener("click",function(){
-document.getElementById("accept").addEventListener("click",function(){	
-
-if(checkForm()==true) {	
-    //console.log("fields have been validated");
-   // alert("Form is valid. Data will be saved, thanks!");	
-	//window.location.href = "configLocations.html";
-	 history.go(-1);
-	}
-else{ 
-  console.log("form no valid, please check name and location coordinates!");
-  alert("form no valid, please check name!");
- } 
-
-});
-
-
-
-//validation process
-function checkForm(){ 
-	
-	var name=document.getElementById("addName").value;	
-
-	
-	//var valid=true;
-	
-	//checks Numerical Values
-	if(name==""){		
-	  console.log("please enter a location name");	 
-	  
-	  return false;
-	}	
-	
-	
-	
-	return true;
-};
-
 //backwards
 back.addEventListener('click', function(){
-  //alert(" go back ");
-  window.location.href = 'configLocations.html';
+  window.location.href = 'configSpecies.html';
 });
 
 cancel.addEventListener('click', function(){
-  window.location.href = 'configLocations.html';
+  window.location.href = 'configSpecies.html';
 });
 
+accept.addEventListener('click', function(){
+  window.location.href = 'configSpecies.html';
+});
