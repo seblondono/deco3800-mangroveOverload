077fb5ec3af8605ca76470442f8cb49f338b18e8
Alex Liu <ljd6106@gmail.com>
2017-06-03 01:03:48 +1000

    no message

--- js/siteReview.js
+++ js/siteReview.js
@@ -2,10 +2,16 @@
 const back = document.querySelector('.back');
 const review = document.querySelector('.yearBtn1');
 const click = document.querySelector('#btn2014');
+const review = document.querySelector('#btn2017');
 
 back.addEventListener('click', function(){
   window.location.href = 'site.html';
 });
+const click = document.getElementById('btn2014');
+
+//back.addEventListener('click', function(){
+ // window.location.href = 'site.html';
+//}
 
 review.addEventListener('click', function(){
   window.location.href = 'siteYearReview.html';
